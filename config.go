93bff085451e4f517a251a6f6d1487eec089ceb6
zhhigh <hdbmaster@139>
2013-12-01 02:25:12 +0800

    first

--- config.go
+++ config.go
@@ -11,9 +11,17 @@ var (
 	//AppName       string
 	AppPath       string
 	AppConfigPath string
-	HttpPort      string
+	//Log conf
 	LogFileName   string
 	LogType       string
+	//weixin conf,read from conf
+	WX_DB        string
+	WX_Collection string
+	WX_Document   string
+	WX_Token      string
+	WX_Port      string
+	WX_Defaultreply  string
+
 	AppConfig     config.ConfigContainer
 
 )
@@ -26,21 +34,47 @@ func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
+/*
+logtype=file
+logfile=test.log
+jobserver= 106.187.94.189:14079
+wx_db= mongodb://redis:redis@192.241.225.170:27017/rrest
+wx_collection= rrest
+wx_document= msg
+wx_token= qingzhonglele
+wx_port= :80
+wx_defaultreply= 欢迎关注！订阅图文信息输入日期，如0901
+*/
+
 func ParseConfig() (err error) {
 	AppConfig, err = config.NewConfig("ini", AppConfigPath)
 	if err != nil {
 		return err
 	} else {
-		if v := AppConfig.String("httpport"); err == nil {
-			HttpPort = v
-		}
 		if v := AppConfig.String("logfile"); err == nil {
 			LogFileName = v
 		}
 		if v := AppConfig.String("logtype"); err == nil {
 			LogType = v
 		}
-
+        if v := AppConfig.String("wx_db"); err == nil {
+			WX_DB = v
+		}
+		if v := AppConfig.String("wx_collection"); err == nil {
+			WX_Collection = v
+		}
+		if v := AppConfig.String("wx_document"); err == nil {
+			WX_Document = v
+		}
+		if v := AppConfig.String("wx_token"); err == nil {
+			WX_Token = v
+		}
+		if v := AppConfig.String("wx_port"); err == nil {
+			WX_Port = v
+		}
+		if v := AppConfig.String("wx_defaultreply"); err == nil {
+			WX_Defaultreply = v
+		}
 	}
 	return nil
 }
