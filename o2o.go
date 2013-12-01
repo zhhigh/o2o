316a83010a2f5233a75f4132f8e560501a8e1893
zhhigh <hdbmaster@139>
2013-12-01 09:34:45 +0800

    first

--- o2o.go
+++ o2o.go
@@ -2,7 +2,12 @@ package o2o
 
 /*date:[2013-11-29]
   ver :0.01
-  author:zhhigh
+  author:zhhigh                        p
+  1、config read & write
+  2、log
+  3、weixin package
+  4、mongodb
+
 */
 
 const VERSION = "0.0.1"
