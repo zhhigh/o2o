8b8fa40b6b37318b7e55b41ebd8ba85e8b6f8b15
zhhigh <zhhigh@163.com>
2014-11-29 16:11:14 +0800

    new

--- pm25.go
+++ pm25.go
@@ -13,22 +13,22 @@ func NewPM25()*PM25{
    return &PM25{}
 }
 
-func (p *PM25)ReadFromPM25WebPage()string{
- 	var doc *goquery.Document
-	var e error
-	if doc, e = goquery.NewDocument("TARGET_URL"); e != nil {
-		panic(e.Error())
-	}
+//func (p *PM25)ReadFromPM25WebPage()string{
+ 	//var doc *goquery.Document
+	//var e error
+	//if doc, e = goquery.NewDocument("TARGET_URL"); e != nil {
+	//	panic(e.Error())
+	//}
 
-	sel2 := doc.Find(`script`).Text()
+	//sel2 := doc.Find(`script`).Text()
 	//matches := regexp.MustCompile(`var\slocalCityData\s\=\s{.*};`).FindAllString(sel2, -1)
 
-	return matches[0]
+	//return matches[0]
 	/*fileName := "pm.json"
 	dstFile,_ := os.Create(fileName)
 
 	defer dstFile.Close()
 
 	dstFile.WriteString(matches[0])*/
-}
+//}
 
